bccf0f2fe5246fc4683afa555d5656e157b288b9
Josh <seapolco@gmail.com>
2022-12-09 22:07:27 +0000

    FC: Setup and linking remote

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar';
+import './App.css'
+
+function App() {
+
+  return (
+    
+      <div className="meme-gen-container">
+        <Navbar/>
+      </div>
+
+   )
+
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Navbar = () => {
+
+
+
+    return (
+        <nav className="navbar">
+            <div className="n-logo-title">
+            <img src="" alt="" className="nav-logo" />
+            <div className="nav-title"></div>
+            </div>
+            <p>React Course - Project 4</p>
+      </nav>
+    )
+
+
+}
+
+export default Navbar
