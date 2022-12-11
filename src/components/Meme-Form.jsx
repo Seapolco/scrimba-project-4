5e9fa7fafbdd3fe81abc1d3a1dd6336bb03f297f
Josh <seapolco@gmail.com>
2022-12-11 18:58:40 +0000

    Slight styling

--- src/components/Meme-Form.jsx
+++ src/components/Meme-Form.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 
 const MemeForm = () => {
 
+    const logCon = (e) => {
+        e.preventDefault();
+        console.log('clicked!')
+    }
+
     return (
-        <div className='form-container'>
+        <form className='form-container'>
             <div className="inputs">
                 <input type="text"  className="input-1" placeholder='Shut up'/>
                 <input type="text" className="input-2" placeholder='and take my money!'/>
-                <button className="new-meme-btn">Get a new meme image  🖼</button>
+                <button onClick={logCon} className="new-meme-btn">Get a new meme image  🖼</button>
             </div>
-        </div>
+        </form>
     )
 }
 
